c0382bff88a5323c7222ee0bdcd9c0b22596812f
MekalaKarthik <mekala.karthik2112@gamil.com>
2024-01-10 16:52:06 +0530

    update

--- app.py
+++ app.py
@@ -1,45 +1,45 @@
-
-#Importing Flask
-
+#pip install flask 
+#import required libraries
 from flask import Flask,render_template,jsonify,request
 import pickle
-
-
-app=Flask(__name__)
-
-
-# Define a route for the home page
+import sqlite3
+app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
 @app.route('/prediction',methods=['GET','POST'])
 def prediction():
-    if request.method=="POST":
-         nitro=request.form.get("nitrogen")
-         phos=request.form.get("phosphorus")
-         potas=request.form.get("potassium")
-         temp=request.form.get("temperature")
-         humi=request.form.get("humidity")
-         ph=request.form.get("ph")
-         rain=request.form.get("rainfall")
-         
-         print(nitro,phos,potas,temp,humi,ph,rain)
-         with open('model.pkl','rb')as model_file:
-          mlmodel=pickle.load(model_file)
-        
-         res=mlmodel.predict([[float(nitro),float(phos),float(potas),float(temp),float(humi),float(ph),float(rain)]])
-         
-         
-         return render_template("result.html",res=res)
-         
+    if request.method=='POST':
+        nitro=request.form.get('nitrogen')
+        phos=request.form.get('phosporus')
+        kp=request.form.get('potassium')
+        temp=request.form.get('temperature')
+        hum=request.form.get('humidity')
+        ph=request.form.get('ph')
+        rain=request.form.get('rainfall')
+        print(nitro,phos,kp,temp,hum,ph,rain)
+        with open('model.pkl','rb') as model_file:
+            mlmodel=pickle.load(model_file)
+        res=mlmodel.predict([[float(nitro),float(phos),float(kp),float(temp),float(hum),float(ph),float(rain)]])
+        print(res)
+        conn=sqlite3.connect('cropdata.db')
+        cur=conn.cursor()
+        cur.execute(f''' insert into crop values({nitro},{phos},{kp},{temp},{hum},{ph},{rain},'{res[0]}')''')
+        conn.commit()
+        return render_template("result.html",res=res[0])
     else:
         return render_template('prediction.html')
-    
-    
 
 
-if __name__ == '__main__':
-    # Run the Flask application
-    app.run(host='0.0.0.0',port =5050)
+@app.route('/showdata',methods=['GET','POST'])
+def showdata():
+    conn=sqlite3.connect('cropdata.db')
+    cur=conn.cursor()
+    cur.execute("select*from crop")
+    print(cur.fetchall())
+    return render_template('showdata.html')
+   
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5050)
